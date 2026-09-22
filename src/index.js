770882b40fd3219b08ba58cca41973afa74ade7d
aswanikumar2323-spec <aswanikumar2323@gmail.com>
2026-09-22 19:27:35 +0530

    add prettier config

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ const app = express()
 app.get('/',(req,res)=>{
     res.send("Server is ready")
 })
+
 const port=process.env.PORT|3000
 app.listen(port, ()=>{
-    console.log(`Serve at http://localhost:${port}`)
+    console.log(`Server at http://localhost:${port}`)
 })
 
